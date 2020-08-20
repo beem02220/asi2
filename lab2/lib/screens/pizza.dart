5413db62a543154e62bb94d2bbacd8a94dfa6d61
beem02220 <nudtee7511@gmail.com>
2020-08-20 15:03:15 +0700

    init repo

---
+++ lab2/lib/screens/pizza.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class MyPizza extends StatefulWidget {
+   final String title;
+
+  const MyPizza({Key key, this.title}) : super(key: key);
+  @override
+  _MyPizzaState createState() => _MyPizzaState();
+}
+
+class _MyPizzaState extends State<MyPizza> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
