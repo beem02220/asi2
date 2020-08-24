6c2b4df8be15bc374c714dc65bb45163244bffdc
beem02220 <nudtee7511@gmail.com>
2020-08-24 20:52:59 +0700

    ver2

--- lab2/lib/screens/pizza.dart
+++ lab2/lib/screens/pizza.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class MyPizza extends StatefulWidget {
-   final String title;
+  final String title;
 
   const MyPizza({Key key, this.title}) : super(key: key);
   @override
@@ -8,10 +9,91 @@ class MyPizza extends StatefulWidget {
 }
 
 class _MyPizzaState extends State<MyPizza> {
+  String groupFood;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            
+            
+            RadioListTile(
+              title: Text('Pizza'),
+              controlAffinity: ListTileControlAffinity.platform,
+              value: 'Pizza',
+              groupValue: groupFood,
+              onChanged: (value) {
+                setState(() {
+                  groupFood = value;
+                });
+              },
+            ),
+            RadioListTile(
+              title: Text('Shabu'),
+              controlAffinity: ListTileControlAffinity.platform,
+              value: 'Shabu',
+              groupValue: groupFood,
+              onChanged: (value) {
+                setState(() {
+                  groupFood = value;
+                });
+              },
+            ),
+          
+           RadioListTile(
+              title: Text('Steak'),
+              controlAffinity: ListTileControlAffinity.platform,
+              value: 'Steak',
+              groupValue: groupFood,
+              onChanged: (value) {
+                setState(() {
+                  groupFood = value;
+                });
+              },
+            ),
+          
+
+
+           RadioListTile(
+              title: Text('Salad'),
+              controlAffinity: ListTileControlAffinity.platform,
+              value: 'Salad',
+              groupValue: groupFood,
+              onChanged: (value) {
+                setState(() {
+                  groupFood = value;
+                });
+              },
+            ),
+          
+          
+            RadioListTile(
+              title: Text('Sandwich'),
+              controlAffinity: ListTileControlAffinity.platform,
+              value: 'Sandwich',
+              groupValue: groupFood,
+              onChanged: (value) {
+                setState(() {
+                  groupFood = value;
+                });
+              },
+            ),
+          
+          
+          
+          
+          
+          
+          
+          
+          ],
+        ),
+      ),
     );
   }
 }
